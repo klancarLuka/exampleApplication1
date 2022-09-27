732fa4f386bb7d63edbea1a8df0755c06e01592b
lk <klancar.luka@gmail.com>
2022-09-27 17:44:31 +0200

    modified:   CMakeLists.txt 	modified:   app/CMakeLists.txt 	new file:   app/boards/nucleo_wb55rg.overlay 	modified:   app/debug.conf 	modified:   app/prj.conf 	modified:   app/rtt.conf 	modified:   app/src/main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,47 +1,102 @@
 /*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
+ 	/* included headers */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
 
-#include "app_version.h"
+	/* button definition */
+	
+#define SW1_NODE DT_ALIAS(sw0) /* sw0 is actually button SW1 on nucleo_wb55rg */
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW1_NODE, gpios, {0});
+static struct gpio_callback button_cb_data;
 
+	/* led gpio & start state definition */
+#define LED DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED, gpios);
+static uint8_t led_state = false;
+
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
+
+                           
 void main(void)
 {
+	/* led config + tests*/
+	if(!device_is_ready(led.port)){
+		printk("Error: Led %s is not ready\n",
+		       led.port);
+	}
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+	gpio_pin_set_dt(&led, led_state);
+
+	/* button config + tests */
 	int ret;
-	const struct device *sensor;
-
-	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
+	if (!device_is_ready(button.port)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return;
+	}
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return;
+	}
+	
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret, button.port->name, button.pin);
 		return;
 	}
 
-	while (1) {
-		struct sensor_value val;
 
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return;
+	if (led.port) {
+		bool previouslyClicked = false;
+		while (1) {
+			int val = gpio_pin_get_dt(&button);
+			if (val >= 0) {
+				if (val == 0) {
+					if (previouslyClicked) { 
+					/*button was pressed and released */
+						previouslyClicked = false;			
+						ret = gpio_pin_toggle_dt(&led);
+						led_state = gpio_pin_get_dt(&led);
+						if (ret < 0) {
+							return;
+						}
+					}					
+				} else {
+					if(!previouslyClicked){
+						previouslyClicked = true;
+					}
+				}
+			}
+			k_msleep(1);
 		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
+		
 	}
 }
+
+
+
+
+
+
+
+
 
