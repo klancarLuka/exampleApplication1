d1bbaf12ee757dfa8628fad3783d22998f15d9e8
lk <klancar.luka@gmail.com>
2022-10-02 22:55:11 +0200

    modified:   app/src/main.c

--- app/src/main.c
+++ app/src/main.c
@@ -22,14 +22,18 @@
 
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW1_NODE, gpios, {0});
 
+static struct gpio_callback button_cb_data; /* callback data definiton */
+
 	/* led gpio & start state definition */
 #define LED DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED, gpios);
-static uint8_t led_state = false;
+static uint8_t led_state = 0;
 
 
-	/* bt init, service & characteristic */
-static struct bt_uuid_128 led_state_char_uuid = BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x9c85a726, 0xb7f1, 0x11ec, 0xb909, 0x0242ac120002));
+	/* bt uuid */
+static struct bt_uuid_128 led_state_char_notify_uuid = BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x9c85a727, 0xb7f1, 0x11ec, 0xb909, 0x0242ac120002));
+static struct bt_uuid_128 led_state_char_read_uuid = BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x9c85a728, 0xb7f1, 0x11ec, 0xb909, 0x0242ac120002));
+
 #define LED_SERVICE_UUID_VAL \
   BT_UUID_128_ENCODE(0xf7547938, 0x68ba, 0x11ec, 0x90d6, 0x0242ac120003)
 
@@ -37,39 +41,100 @@ static struct bt_uuid_128 led_svc_uuid =
 	BT_UUID_INIT_128(LED_SERVICE_UUID_VAL);
 	
 	/* advertisement data */
-	
+
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
     BT_DATA_BYTES(BT_DATA_UUID128_ALL, LED_SERVICE_UUID_VAL),
 };
 
+/* read state function */
 static ssize_t read_led_state(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset) {
-  const uint8_t *val = attr->user_data;
-  printk("Value 0x%x read.\n", *val);
-  return bt_gatt_attr_read(conn, attr, buf, len, offset, val, sizeof(*val));
+
+  char izhod[11] = "LED is OFF"; //size("LED is OFF" + null char) = 11 
+
+  if (led_state == 0){
+	izhod[8] = 'F';
+	izhod[9] = 'F';
+  } else {
+ 	izhod[9] = ' ';
+	izhod[8] = 'N';
+  }
+
+  return bt_gatt_attr_read(conn, attr, buf, len, offset, izhod, sizeof(izhod));
 }
 
-BT_GATT_SERVICE_DEFINE(
-    led_svc, BT_GATT_PRIMARY_SERVICE(&led_svc_uuid),
-    BT_GATT_CHARACTERISTIC(&led_state_char_uuid.uuid,
+/* change indication function */
+static uint8_t simulate_vnd;
+
+static void led_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value) {
+	simulate_vnd = (value == BT_GATT_CCC_INDICATE) ? 1 : 0;
+}
+
+/* bt service definition */
+BT_GATT_SERVICE_DEFINE(led_svc, BT_GATT_PRIMARY_SERVICE(&led_svc_uuid),
+	BT_GATT_CHARACTERISTIC(&led_state_char_notify_uuid.uuid,	/*notify chrc*/
+                           BT_GATT_CHRC_NOTIFY,
+                           BT_GATT_PERM_NONE,
+                           NULL, NULL, NULL),
+	BT_GATT_CCC(led_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CHARACTERISTIC(&led_state_char_read_uuid.uuid,		/* read chrc*/
                            BT_GATT_CHRC_READ,
                            BT_GATT_PERM_READ,
-                           read_led_state, NULL, &led_state), );
+                           read_led_state, NULL, &led_state));
+  
+ /* calback functions */           
+ static void connected (struct bt_conn *conn, uint8_t err){
+ 	if(err){
+ 		printk("Connection failed\n");
+ 	} else {
+ 		printk("Connected\n");
+ 	}
+ }
+ static void disconnected (struct bt_conn *conn, uint8_t reason){
+	printk("Disconnected\n");
+ }
+ 
+ static struct bt_conn_cb conn_callbacks = {
+ 	.connected = connected,
+ 	.disconnected = disconnected,
+};
               
-              
+/* notification function */
+int bt_notify(){
+
+	char izhod[11] = "LED is OFF"; //size("LED is OFF" + null char) = 11 
+
+	if (led_state == 0){
+		izhod[8] = 'F';
+		izhod[9] = 'F';
+	} else {
+		izhod[9] = ' ';
+		izhod[8] = 'N';
+	}
+	int rc;
+	rc = bt_gatt_notify(NULL, &led_svc.attrs[1], izhod, sizeof(izhod));
+	
+	return rc == -ENOTCONN ? 0 : rc;
+}
+      
+/* on interupt function */
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
 		    uint32_t pins)
 {
 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	gpio_pin_toggle_dt(&led);
+	led_state = (led_state + 1) % 2;
+	
+	if (bt_notify() != 0){
+		printk("Notification error\n");
+	}
 }
-
-                           
+                     
 void main(void)
 {
 	/* led config + tests*/
 	if(!device_is_ready(led.port)){
-		printk("Error: Led is not ready\n",
-		       led.port);
+		printk("Error: Led is not ready\n");
 	}
 	gpio_pin_configure_dt(&led, GPIO_OUTPUT);
 	gpio_pin_set_dt(&led, led_state);
@@ -95,6 +160,9 @@ void main(void)
 		return;
 	}
 	
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	
 	
 	/* Bluetooth config + tests */
 	int err;
@@ -117,29 +185,14 @@ void main(void)
 	}
 	printk("Advertising successfully started\n");
 	
-
+	/* callback registration */
+	bt_conn_cb_register(&conn_callbacks);
+	
 	if (led.port) {
-		bool previouslyClicked = false;
 		while (1) {
-			int val = gpio_pin_get_dt(&button);
-			if (val >= 0) {
-				if (val == 0) {
-					if (previouslyClicked) { 
-					/*button was pressed and released */
-						previouslyClicked = false;			
-						ret = gpio_pin_toggle_dt(&led);
-						led_state = gpio_pin_get_dt(&led);
-						if (ret < 0) {
-							return;
-						}
-					}					
-				} else {
-					if(!previouslyClicked){
-						previouslyClicked = true;
-					}
-				}
-			}
-			k_msleep(1);
+			/* empty main loop */
+			/* additionaly, we could add a delay 
+			for lower power consumption */
 		}
 		
 	}
